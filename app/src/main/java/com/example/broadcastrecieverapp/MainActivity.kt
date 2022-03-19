c7ff1c3546788fabfda92f45604c34017c26dace
Alexander <alez.grante@gmail.com>
2022-03-20 01:45:15 +0300

    local broadCast receiver!

--- app/src/main/java/com/example/broadcastrecieverapp/MainActivity.kt
+++ app/src/main/java/com/example/broadcastrecieverapp/MainActivity.kt
@@ -5,12 +5,15 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.broadcastrecieverapp.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
     private val myBroadcastReceiver: BroadcastReceiver = MyRegisteredByCodeBroadcastReceiver()
+
+    private val myLocalBroadcastReceiver: BroadcastReceiver = MyLocalBroadcastReceiver()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +22,9 @@ class MainActivity : AppCompatActivity() {
 
         val intentFilter = IntentFilter("MY_ACTION")
         this.registerReceiver(myBroadcastReceiver,intentFilter)
+
+        LocalBroadcastManager.getInstance(this)
+            .registerReceiver(myLocalBroadcastReceiver, IntentFilter("MY_LOCAL_ACTION"))
 
         binding.button.setOnClickListener {
             val intent = Intent()
@@ -33,12 +39,19 @@ class MainActivity : AppCompatActivity() {
             sendBroadcast(intent)
         }
 
+        binding.buttonBcrLocal.setOnClickListener {
+            val intent = Intent("MY_LOCAL_ACTION")
+            intent.putExtra("data", "Hello local World!")
+            LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
+        }
+
         setContentView(binding.root)
     }
 
     override fun onStop() {
         super.onStop()
         unregisterReceiver(myBroadcastReceiver)
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(myBroadcastReceiver)
     }
 
 }
