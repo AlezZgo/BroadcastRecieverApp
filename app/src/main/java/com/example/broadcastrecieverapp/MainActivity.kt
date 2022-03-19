b200bf804ac5b8730ecc57ccbb32eba06f1bbd05
Alexander <alez.grante@gmail.com>
2022-03-20 01:24:21 +0300

    custom broadcast with declaration in manifest

--- app/src/main/java/com/example/broadcastrecieverapp/MainActivity.kt
+++ app/src/main/java/com/example/broadcastrecieverapp/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.broadcastrecieverapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.broadcastrecieverapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.button.setOnClickListener {
+
+                val intent = Intent()
+                intent.action = "com.example.broadcast.MY_NOTIFICATION"
+                intent.putExtra("data", "Hello World!")
+                sendBroadcast(intent)
+            }
+
+        setContentView(binding.root)
     }
+
 }
---
+++ app/src/main/java/com/example/broadcastrecieverapp/MyBroadcastReciever.kt
@@ -0,0 +1,16 @@
+package com.example.broadcastrecieverapp
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class MyBroadcastReciever: BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val data = intent?.extras?.getString("data");
+        Toast.makeText(context, "Broadcast Received with data " + data, Toast.LENGTH_LONG).show();
+    }
+
+
+}
